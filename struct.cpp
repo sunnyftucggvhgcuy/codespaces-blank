203c9f40b405bf9961053402b689e611501674e0
sunnyftucggvhgcuy <jey.jacky.jun@gmail.com>
2025-03-06 19:52:20 +0800

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+
+
 struct Student {
     int scores[5]; 
     int average;   
@@ -7,30 +9,42 @@ struct Student {
 
 int main() {
     int n;
-    cin >> n;
-    vector<Student> students(n);
+    std::cin >> n;
+    
+    
+    std::vector<Student> students(n);
+    
+    
     int subjectSums[5] = {0};
+    
+    
     for (int i = 0; i < n; i++) {
+        
         for (int j = 0; j < 5; j++) {
-            cin >> students[i].scores[j];
+            std::cin >> students[i].scores[j];
+            
             subjectSums[j] += students[i].scores[j];
         }
+        
+        
         int sum = 0;
         for (int j = 0; j < 5; j++) {
             sum += students[i].scores[j];
         }
         students[i].average = sum / 5;
         
-        cout << students[i].average << std::endl;
+        
+        std::cout << students[i].average << std::endl;
     }
-        for (int j = 0; j < 5; j++) {
-        cout << subjectSums[j] / n;
+    
+    
+    for (int j = 0; j < 5; j++) {
+        std::cout << subjectSums[j] / n;
         if (j < 4) {
-            cout << " ";
+            std::cout << " ";
         }
     }
-    cout << std::endl;
+    std::cout << std::endl;
     
     return 0;
 }
-    
